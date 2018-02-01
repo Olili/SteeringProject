c44103b60130674c0e97b83999f0bdfcdd020998
Unknown <olivier.serris@wanadoo.fr>
2018-02-01 13:42:05 +0100

    Add Obstacle Avoidance Scenario

--- Assets/Script/Entity/Agent.cs
+++ Assets/Script/Entity/Agent.cs
@@ -13,12 +13,17 @@ public class Agent : MonoBehaviour {
 	void FixedUpdate () {
         steering.Seek(transform.position + transform.forward);
         steering.Move();
+        transform.forward = GetComponent<Rigidbody>().velocity;
     }
     private void Update()
     {
-        if (transform.position.z > 24)
-        {
+        if (transform.position.z > 24.5f)
             transform.position = new Vector3(transform.position.x, transform.position.y, -24);
-        }
+        else if (transform.position.z < -24.5f)
+            transform.position = new Vector3(transform.position.x, transform.position.y, 24);
+        else if (transform.position.x > 24.5f)
+            transform.position = new Vector3(-24, transform.position.y, transform.position.z);
+        if (transform.position.x < -24.5f)
+            transform.position = new Vector3(24, transform.position.y, transform.position.z);
     }
 }
