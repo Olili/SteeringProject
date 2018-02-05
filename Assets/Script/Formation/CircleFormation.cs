fc5c42dd22f1335faa5af33dd0f8981c81d47147
Unknown <olivier.serris@wanadoo.fr>
2018-02-05 21:42:34 +0100

    Reorganse Formation Script + add CircleFormation

---
+++ Assets/Script/Formation/CircleFormation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleFormation : Formation
+{
+    public override Vector3 GetSlot(Vector3 position)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override  Vector3 GetSlotPos(Vector3 origin, Quaternion orientation, int nbSlots, int i)
+    {
+        float deltaA = 2 * (float)Math.PI / nbSlots;
+        float ray = nbSlots * 0.5f; // should use entity Size.
+        return new Vector3(Mathf.Cos(deltaA * i) * ray, 0, Mathf.Sin(deltaA * i) * ray);
+    }
+
+    [SerializeField] int nbSlots = 5;
+    public void Update() // for debug Purposes
+    {
+        UpdateFormation(Vector3.zero, Quaternion.identity, nbSlots);
+    }
+    public void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        if (slots != null)
+            for (int i = 0; i < slots.Count; i++)
+                Gizmos.DrawSphere(slots[i].position, 0.25f);
+    }
+}
--- Assets/Script/Formation/Formation.cs
+++ Assets/Script/Formation/Formation.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections;
+﻿
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -15,6 +14,7 @@ abstract public class Formation : MonoBehaviour {
     {
         public Vector3 position;
         public bool occupied;
+        public Slot(Vector3 _pos) { position = _pos; occupied = false; }
     }
     protected List<Slot> slots;
     public void Awake()
@@ -22,35 +22,25 @@ abstract public class Formation : MonoBehaviour {
         slots = new List<Slot>();
     }
     abstract public Vector3 GetSlot(Vector3 position);
-    abstract public void UpdateFormation(Vector3 origin, Quaternion orientation,int nbSlots);
-}
-public class CircleFormation : Formation
-{
-    public override Vector3 GetSlot(Vector3 position)
+    public virtual void UpdateFormation(Vector3 origin, Quaternion rotation, int nbSlots)
     {
-        throw new NotImplementedException();
+            // Check if slot need update
+        if (transform.position == origin && rotation == transform.rotation && nbSlots == slots.Count)
+            return;
+        if (nbSlots <= 0)
+            return;
+            // remove unnecessary slots
+        for (int i = slots.Count-1; i >= nbSlots; i--)
+            slots.RemoveAt(i);
+            // updateSlots
+        for (int i = 0; i < nbSlots; i++)
+        {
+            if (i < slots.Count)
+                slots[i].position = GetSlotPos(origin, rotation, nbSlots, i);
+            else
+                slots.Add(new Slot(GetSlotPos(origin, rotation, nbSlots, i)));
+        }
     }
 
-    public override void UpdateFormation(Vector3 origin, Quaternion orientation, int nbSlots)
-    {
-        if (transform.position == origin && orientation == transform.rotation && nbSlots == slots.Count)
-            return;
-        float angle = 2*Mathf.PI / slots.Count;
-        for (int i = 0; i < slots.Count;i++)
-        {
-            //slots[i].position = new Vector3(Mathf.Cos(angle), 0, Mathf.Sin(angle));
-            slots[i].occupied = false;
-        }
-    }
-    public void OnDrawGizmosSelected()
-    {
-        if (slots!=null)
-        {
-            for (int i = 0; i < slots.Count; i++)
-            {
-                Gizmos.DrawSphere(slots[i].position, 1);
-            }
-        }
-    }
-    
+    abstract protected Vector3 GetSlotPos(Vector3 origin, Quaternion orientation,int nbSlots,int i);
 }
