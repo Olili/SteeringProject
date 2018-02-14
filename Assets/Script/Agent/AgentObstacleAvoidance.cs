583654fa1002ffdbf7f9fa3e4ed3626e1763ebc4
Unknown <olivier.serris@wanadoo.fr>
2018-02-14 12:52:57 +0100

    enhanced scenario

--- Assets/Script/Agent/AgentObstacleAvoidance.cs
+++ Assets/Script/Agent/AgentObstacleAvoidance.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class AgentObstacleAvoidance : Agent {
 
+    Transform target;
     public void Start()
     {
-        steering.AddBehavior(new Wander(steering));
+      
+        target = new GameObject("target").transform;
+        target.position = transform.position + Vector3.forward * 2;
+        target.parent = transform;
+
+        steering.AddBehavior(new Seek(steering,target));
+        steering.AddBehavior(new Separation(steering));
         steering.AddBehavior(new ObstacleAvoidance(steering));
     }
     void FixedUpdate () {
         //steering.Seek(transform.position + transform.forward);
         //steering.ObstaclesAvoidance();
         //steering.Move();
-        //if (rb.velocity!=Vector3.zero)
-        //    transform.forward = rb.velocity;
+        Vector3 velocity = rb.velocity;
+        velocity.y = 0;
+        if (velocity != Vector3.zero)
+            transform.forward = velocity;
     }
 }
---
+++ Assets/Script/Utils/AnimationHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationHandler : MonoBehaviour {
+
+    Animator animator;
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 velocity = GetComponent<Rigidbody>().velocity;
+        animator.SetFloat("Speed", velocity.magnitude);
+        velocity.y = 0;
+        if (velocity != Vector3.zero)
+            transform.forward = velocity;
+    }
+}
