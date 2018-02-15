3bbf67f63dfe666cf9078adc51879e879978672a
Unknown <olivier.serris@wanadoo.fr>
2018-02-15 01:34:37 +0100

    walkOnWWall Agent demo (WIp)

--- Assets/Scene/Scenario/WalkOnWall/SurfaceAgent.cs
+++ Assets/Scene/Scenario/WalkOnWall/SurfaceAgent.cs
@@ -4,18 +4,19 @@ using UnityEngine;
 
 public class SurfaceAgent : Agent {
 
-    Transform target;
+    [SerializeField] Transform target;
     public void Start()
     {
-        target = new GameObject("target").transform;
-        target.position = transform.position + Vector3.forward * 5;
-        target.parent = transform;
+        //target = new GameObject("target").transform;
+        //target.position = transform.position + Vector3.forward * 50;
+        //target.parent = transform;
 
         steering.AddBehavior(new Seek(steering, target));
+        steering.AddBehavior(new Separation(steering));
         //steering.AddBehavior(new Separation(steering));
     }
     public void Update()
     {
-        target.position = transform.position + Vector3.forward * 5 - Vector3.up* transform.position.y;
+        //target.position = transform.position + Vector3.forward * 50 - Vector3.up* transform.position.y;
     }
 }
--- Assets/Script/Steering/SurfaceSteering.cs
+++ Assets/Script/Steering/SurfaceSteering.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SurfaceSteering : Steering {
 
-     // Detect front object to walk on
+     // Detect front/back object to walk on
     float maxFaceRayDistance;
     float faceSphereCastRadius;
         //Detect down object to walk on
@@ -16,7 +16,7 @@ public class SurfaceSteering : Steering {
     float maxCatRotation;   // rotate when not touching ground to land on feet.
 
     float positionOnTerrainSpeed; // speed at wich the entity replace itself according to slope.
-
+    Vector3 onCollisonNormal;
     Quaternion newRotation;
     LayerMask rayMask;
     public override void Awake()
@@ -24,14 +24,14 @@ public class SurfaceSteering : Steering {
         base.Awake();
         string[] layerNametab = { "Default", "Ground", "Obstacle" };
         rayMask.value = LayerMask.GetMask(layerNametab);
-        faceSphereCastRadius = 0.2f;
-        downSphereCastRadius = 0.3f;
+        faceSphereCastRadius = 0.1f;
+        downSphereCastRadius = 0.2f;
         maxRotationOnTerrain = 400;
         maxRotationDirection = 200;
         maxCatRotation = 500;
 
         positionOnTerrainSpeed = 3;
-        maxFaceRayDistance = 2;
+        maxFaceRayDistance = 1.2f;
         maxDownRayDistance = agentCollExtent.y * 0.5f;
 
         newRotation = transform.rotation;
@@ -49,7 +49,6 @@ public class SurfaceSteering : Steering {
         RaycastHit backHit = new RaycastHit();
         bool hasFrontHit = false;
         bool hasBackHit = false;
-        Vector3 averageNormal = Vector3.Lerp(OnPlanNormal, Vector3.up,0.1f);
 
             // Compute object hitted
         hasFrontHit = ComputeFrontHit(out frontHit);
@@ -58,29 +57,33 @@ public class SurfaceSteering : Steering {
         IsOnGround = true;
 
         if (hasFrontHit && hasBackHit)
-            averageNormal = PositionOnTerrain(frontHit, backHit);
+            OnPlanNormal = PositionOnTerrain(frontHit, backHit);
         else if (hasFrontHit)
-            averageNormal = PositionOnTerrain(frontHit, false);
+            OnPlanNormal = PositionOnTerrain(frontHit, false);
         else if (hasBackHit)
-            averageNormal = PositionOnTerrain(backHit, true);
+            OnPlanNormal = PositionOnTerrain(backHit, true);
         else
         {
-            //if (!DebugCenterRay())
+            if (!isColliding)
             {
                 Debug.Log("Fall");
-                RotateToNormalGround();
-                Rb.MoveRotation(newRotation);
                 IsOnGround = false;
-                OnPlanNormal = averageNormal;
+                OnPlanNormal = Vector3.Lerp(OnPlanNormal, Vector3.up, 0.1f);
+                RotateToNormalGround(OnPlanNormal);
+                //Rb.MoveRotation(newRotation);
             }
-            return;
+            else
+            {
+                OnPlanNormal = onCollisonNormal;
+                transform.position += onCollisonNormal * 0.2f;
+            }
+            //return;
         }
-        OnPlanNormal = averageNormal;
 
             // compute all steering force and apply to velocity
         base.FixedUpdate();
-
-        RotateToVelocity(averageNormal);
+        RotateOnPlan(OnPlanNormal);
+        RotateToVelocity(OnPlanNormal);
         Rb.MoveRotation(newRotation);
     }
 
@@ -88,27 +91,30 @@ public class SurfaceSteering : Steering {
     private bool ComputeFrontHit(out RaycastHit frontHit)
     {
         Vector3 transformUp = transform.up;
-        Vector3 offsetZ = transform.forward * (AgentCollExtent.z+1);
+        Vector3 offsetZ = transform.forward * maxFaceRayDistance*0.50f;
         bool hasHit = false;
         /* SphereCast face au monstre
         On verifie que le monstre ne doit pas monter face à une obstruction. */
         Ray faceRay = new Ray(transform.position + transform.up* faceSphereCastRadius*1.1f, transform.forward);
-        Debug.DrawLine(faceRay.origin, faceRay.origin+ faceRay.direction*(maxFaceRayDistance+ faceSphereCastRadius),Color.red);
-        if (!(hasHit = Physics.SphereCast(faceRay, faceSphereCastRadius, out frontHit,
-            maxFaceRayDistance - faceSphereCastRadius, rayMask.value,QueryTriggerInteraction.Ignore)))
+        Debug.DrawLine(faceRay.origin, faceRay.origin+ faceRay.direction*maxFaceRayDistance, Color.red);
+        if (!(hasHit = Physics.Raycast(faceRay, out frontHit,
+            maxFaceRayDistance , rayMask.value,QueryTriggerInteraction.Ignore)))
         {
             /* BoxCast front down.  
             On essaie de récupérer ce sur quoi le monster marche en face de lui.*/
             Ray frontDownRay = new Ray(faceRay.origin + offsetZ, -transformUp);
-            if (!(hasHit = Physics.SphereCast(frontDownRay, downSphereCastRadius, out frontHit,
+            Debug.DrawLine(frontDownRay.origin, frontDownRay.origin + frontDownRay.direction * maxDownRayDistance, Color.blue);
+            if (!(hasHit = Physics.Raycast(frontDownRay, out frontHit,
                 maxDownRayDistance , rayMask.value, QueryTriggerInteraction.Ignore)))
             {
                 /* Raycast front down.  
                 On essaie de récupérer ce sur quoi le monster marche en face de lui.*/
-                Vector3 frontDownRayPosition = faceRay.origin - (transform.up * AgentCollExtent.y) + (transform.forward * AgentCollExtent.z );
+                Vector3 frontDownRayPosition = frontDownRay.origin + frontDownRay.direction* maxDownRayDistance;
                 Ray frontDowBack = new Ray(frontDownRayPosition, -transform.forward);// check Raycast front down au monstre
+                Debug.DrawLine(frontDowBack.origin, frontDowBack.origin + frontDowBack.direction * maxDownRayDistance, Color.yellow);
+
                 hasHit = Physics.Raycast(frontDowBack, out frontHit, maxDownRayDistance, rayMask.value,QueryTriggerInteraction.Ignore);
-                frontHit.point = frontDownRay.origin + frontDownRay.direction * maxDownRayDistance;
+                //frontHit.point = frontDownRay.origin /*+ frontDownRay.direction * maxDownRayDistance*/;
             }
         }
         return hasHit;
@@ -117,14 +123,17 @@ public class SurfaceSteering : Steering {
     {
         bool hasHit = false;
         Vector3 transformUp = transform.up;
-        Vector3 offsetZ = transform.forward * maxFaceRayDistance;
+        Vector3 offsetZ = transform.forward * maxFaceRayDistance * 0.50f;
 
         Ray backRay = new Ray(transform.position + transform.up * faceSphereCastRadius * 1.1f, -offsetZ);
-        if (!(hasHit = Physics.SphereCast(backRay, faceSphereCastRadius, out backHit,
-            maxFaceRayDistance - faceSphereCastRadius, rayMask.value, QueryTriggerInteraction.Ignore)))
+        Debug.DrawLine(backRay.origin, backRay.origin + backRay.direction * maxFaceRayDistance, Color.red);
+
+        if (!(hasHit = Physics.Raycast(backRay, out backHit,
+            maxFaceRayDistance, rayMask.value, QueryTriggerInteraction.Ignore)))
         {
             Ray backDownRay = new Ray(backRay.origin - offsetZ, -transformUp);
-            hasHit = Physics.SphereCast(backDownRay, downSphereCastRadius, out backHit,
+            Debug.DrawLine(backDownRay.origin, backDownRay.origin + backDownRay.direction * maxDownRayDistance, Color.blue);
+            hasHit = Physics.Raycast(backDownRay, out backHit,
                 maxDownRayDistance, rayMask.value, QueryTriggerInteraction.Ignore);
         }
 
@@ -134,12 +143,11 @@ public class SurfaceSteering : Steering {
 
     private Vector3 PositionOnTerrain(RaycastHit frontHit, RaycastHit backHit)
     {
-        Vector3 averageNormal = (frontHit.normal * 0.65f + backHit.normal * 0.35f);
-        Vector3 averageHitPosition = (frontHit.point + backHit.point) / 2.0f;
+        Vector3 averageNormal = frontHit.normal;/// (frontHit.normal + backHit.normal )*0.5f;
+        Vector3 averageHitPosition = (frontHit.point + backHit.point) *0.5f;
         float step = Time.fixedDeltaTime * positionOnTerrainSpeed;
         float distance = (averageHitPosition - transform.position).magnitude;
-        RotateOnPlan(averageNormal);
-        Vector3 finalPosition = Vector3.MoveTowards(transform.position, averageHitPosition , step);
+        Vector3 finalPosition = Vector3.MoveTowards(transform.position, averageHitPosition + averageNormal * 0.1f , step);
 
         Rb.MovePosition(finalPosition);
         return averageNormal;
@@ -148,15 +156,14 @@ public class SurfaceSteering : Steering {
     {
         Vector3 averageNormal;
         if (isBack)
-            averageNormal = (hit.normal + transform.up) / 2.0f;
+            averageNormal = (hit.normal + OnPlanNormal) / 2.0f;
         else
-            averageNormal = hit.normal;
+            averageNormal = (hit.normal + OnPlanNormal) / 2.0f;
 
         float step = Time.fixedDeltaTime * positionOnTerrainSpeed;
-        RotateOnPlan(averageNormal);
-        Vector3 finalPosition = Vector3.MoveTowards(transform.position, hit.point, step);
+        Vector3 finalPosition = Vector3.MoveTowards(transform.position, hit.point+ averageNormal * 0.1f, step);
 
-        Rb.MovePosition(finalPosition);
+        //Rb.MovePosition(finalPosition);
         return averageNormal;
     }
     ////////////// Rotate Creature according to surface walked on :: 
@@ -178,10 +185,10 @@ public class SurfaceSteering : Steering {
             newRotation = finalRotation;
         }
     }
-    private void RotateToNormalGround() // rotate pour retomber sur ses pieds
+    private void RotateToNormalGround(Vector3 normal) // rotate pour retomber sur ses pieds
     {
-        Vector3 planFoward = GetPlanDirection(transform.forward, Vector3.up);
-        Quaternion targetNormaRotation = Quaternion.LookRotation(planFoward, Vector3.up);
+        Vector3 planFoward = GetPlanDirection(transform.forward, normal);
+        Quaternion targetNormaRotation = Quaternion.LookRotation(planFoward, normal);
         Quaternion finalRotation = Quaternion.RotateTowards(newRotation, targetNormaRotation, Time.fixedDeltaTime * maxCatRotation);
         newRotation = finalRotation;
     }
@@ -219,6 +226,7 @@ public class SurfaceSteering : Steering {
     }
     public void OnCollisionStay(Collision coll)
     {
+        onCollisonNormal = coll.contacts[0].normal;
         isColliding = true;
     }
     public void OnCollisionExit(Collision coll)
